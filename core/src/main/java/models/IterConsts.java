d43c684d64ed639855a86529d866f96d9c6ad062
Alexander <texhobeer95@gmail.com>
2017-02-23 21:01:22 +0300

    implemented iterational constants

---
+++ core/src/main/java/models/IterConsts.java
@@ -0,0 +1,23 @@
+package models;
+
+import services.Permutation;
+
+/**
+ * Created by Alexander on 23.02.2017.
+ */
+public class IterConsts {
+    private static final Block[] consts = new Block[32];
+
+    static {
+        for (int i = 0; i < 32; ++i) {
+            consts[i] = new Block(i);
+            byte[] arr = consts[i].toHexArray();
+            Permutation.replace16Times(arr);
+            consts[i].merge(arr);
+        }
+    }
+
+    public static Block get(int i) {
+        return consts[i];
+    }
+}
