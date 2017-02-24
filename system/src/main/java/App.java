bda0ddec1ca2da17057a58ba093a316799afded7
igoryan17 <yadrov@phystech.edu>
2017-02-24 17:10:26 +0300

    create execution module

---
+++ system/src/main/java/App.java
@@ -0,0 +1,8 @@
+/**
+ * Created by igoryan on 24.02.2017.
+ */
+public class App {
+    public static void main(String[] args) {
+        String hexInput = "1122334455667700ffeeddccbbaa9988";
+    }
+}
