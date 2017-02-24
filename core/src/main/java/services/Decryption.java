4fc047637a4e0714acfd1e91f4361095187eb9e6
texhobeer <texhobeer95@gmail.com>
2017-02-24 17:25:36 +0300

    Fixed decryption

--- core/src/main/java/services/Decryption.java
+++ core/src/main/java/services/Decryption.java
@@ -1,15 +1,15 @@
 package services;
 
 import models.Block;
-import models.Data;
 
 import java.util.BitSet;
+import java.util.List;
 
 /**
  * Created by Alexander on 24.02.2017.
  */
 public class Decryption {
-    public static Data decrypt(Block K1, Block K2, Data data) {
+    public static List<Block> decrypt(Block K1, Block K2, List<Block> data) {
         Block[] keys = new Block[10];
         keys[0] = new Block((BitSet) K1.getBits().clone());
         keys[1] = new Block((BitSet) K2.getBits().clone());
@@ -20,7 +20,7 @@ public class Decryption {
         }
 
         for (int i = 9; i >= 0; --i) {
-            for (int j = 0; j < data.length(); j++) {
+            for (int j = 0; j < data.size(); j++) {
                 if (i != 0) {
                     data.set(j, srlrx(data.get(j), keys[i]));
                 }
--- core/src/test/java/services/TestDecryption.java
+++ core/src/test/java/services/TestDecryption.java
@@ -1,9 +1,11 @@
 package services;
 
 import models.Block;
-import models.Data;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Alexander on 24.02.2017.
@@ -12,7 +14,8 @@ public class TestDecryption {
 
     @Test
     public void TestFromGost() {
-        Data data = new Data(new Block("7f679d90bebc24305a468d42b9d4edcd"));
+        List<Block> data = new ArrayList<>();
+        data.add(new Block("7f679d90bebc24305a468d42b9d4edcd"));
         Block k1 = new Block("8899aabbccddeeff0011223344556677");
         Block k2 = new Block("fedcba98765432100123456789abcdef");
         Block exp = new Block("1122334455667700ffeeddccbbaa9988");
